96710b2e976a9a0c2e3315773381d81c441c668e
Niklas <niklas.baumgarten@web.de>
2016-07-28 17:49:05 +0200

    pillow_checkout

---
+++ getting_started.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 20 15:54:43 2016
+
+@author: niklas
+"""
+
+from PIL import Image
+
+bogen1 = Image.open('Bogen1.jpg')
+bogen1.show()
+#bogen1.save()
